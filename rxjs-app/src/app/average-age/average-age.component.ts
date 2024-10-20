f5968f178b38785f7747d36642cf227987a4f7b9
Robert <robert.arciszewski8@gmail.com>
2024-10-20 07:55:16 +0200

    AverageAgeComponent added to app.component.ts

--- rxjs-app/src/app/average-age/average-age.component.ts
+++ rxjs-app/src/app/average-age/average-age.component.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
+import { from } from 'rxjs';
+import { filter, map, mergeMap, toArray } from 'rxjs/operators';
 
 @Component({
   selector: 'app-average-age',
-  standalone: true,
-  imports: [],
   templateUrl: './average-age.component.html',
-  styleUrl: './average-age.component.css'
+  styleUrls: ['./average-age.component.css']
 })
 export class AverageAgeComponent {
 
+  averageAge: number | null = null;  // Przechowujemy wynik średniej
+
+  persons = [
+    { id: 1, name: "Jan Kowalski" },
+    { id: 2, name: "John Doe" },
+    { id: 3, name: "Jarek Kaczka" }
+  ];
+
+  ages = [
+    { person: 1, age: 18 },
+    { person: 2, age: 24 },
+    { person: 3, age: 666 }
+  ];
+
+  locations = [
+    { person: 1, country: "Poland" },
+    { person: 3, country: "Poland" },
+    { person: 1, country: "USA" }
+  ];
+
+  calculateAverageAge() {
+    const polishResidents$ = from(this.locations).pipe(
+      filter(location => location.country === 'Poland'),
+      mergeMap(location => from(this.persons).pipe(
+        filter(person => person.id === location.person),
+        mergeMap(person => from(this.ages).pipe(
+          filter(age => age.person === person.id),
+          map(age => age.age)
+        ))
+      )),
+      toArray()
+    );
+
+    polishResidents$.subscribe((agesArray) => {
+      const sum = agesArray.reduce((total, age) => total + age, 0);
+      const avg = sum / agesArray.length;
+      this.averageAge = avg;  // Zapisz średnią do zmiennej
+    });
+  }
 }
