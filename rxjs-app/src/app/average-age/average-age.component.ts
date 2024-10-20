ccc9e57d87e35f168a369b41f302bbc45738b6ba
Robert <robert.arciszewski8@gmail.com>
2024-10-20 07:51:06 +0200

    component average-age generated

---
+++ rxjs-app/src/app/average-age/average-age.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-average-age',
+  standalone: true,
+  imports: [],
+  templateUrl: './average-age.component.html',
+  styleUrl: './average-age.component.css'
+})
+export class AverageAgeComponent {
+
+}
