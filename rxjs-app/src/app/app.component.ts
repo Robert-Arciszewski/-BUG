9e9c27d3ffa9848e8e8f97bd44e1e66ad0bf2a4b
Robert <robert.arciszewski8@gmail.com>
2024-10-20 08:08:26 +0200

    added stand-alone `AverageAgeComponent`

--- rxjs-app/src/app/app.component.ts
+++ rxjs-app/src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AverageAgeComponent } from './average-age/average-age.component'; // Importujemy komponent
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [AverageAgeComponent],
 })
-export class AppComponent {
-  title = 'rxjs-app';
-}
+export class AppComponent {}
--- rxjs-app/src/app/average-age/average-age.component.ts
+++ rxjs-app/src/app/average-age/average-age.component.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
+import { from } from 'rxjs';
+import { filter, map, mergeMap, toArray } from 'rxjs/operators';
 
 @Component({
   selector: 'app-average-age',
-  standalone: true,
-  imports: [],
   templateUrl: './average-age.component.html',
-  styleUrl: './average-age.component.css'
+  styleUrls: ['./average-age.component.css'],
+  standalone: true,
 })
 export class AverageAgeComponent {
+  averageAge: number | null = null;
 
+  persons = [
+    { id: 1, name: "Jan Kowalski" },
+    { id: 2, name: "John Doe" },
+    { id: 3, name: "Jarek Kaczka" }
+  ];
+
+  ages = [
+    { person: 1, age: 18 },
+    { person: 2, age: 24 },
+    { person: 3, age: 666 }
+  ];
+
+  locations = [
+    { person: 1, country: "Poland" },
+    { person: 3, country: "Poland" },
+    { person: 1, country: "USA" }
+  ];
+
+  calculateAverageAge() {
+    const polishResidents$ = from(this.locations).pipe(
+      filter(location => location.country === 'Poland'),
+      mergeMap(location => from(this.persons).pipe(
+        filter(person => person.id === location.person),
+        mergeMap(person => from(this.ages).pipe(
+          filter((age: { person: number, age: number }) => age.person === person.id),
+          map((age: { person: number, age: number }) => age.age)
+        ))
+      )),
+      toArray()
+    );
+
+    polishResidents$.subscribe((agesArray: number[]) => {
+      const sum = agesArray.reduce((total: number, age: number) => total + age, 0);
+      const avg = sum / agesArray.length;
+      this.averageAge = avg;
+    });
+  }
 }
