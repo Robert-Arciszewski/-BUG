dcabc1fac8c3c486c5cfd546a11271f4d1a54e60
Robert <robert.arciszewski8@gmail.com>
2024-10-20 21:01:31 +0200

    added register possibility

--- django-app/users/forms.py
+++ django-app/users/forms.py
@@ -4,7 +4,6 @@ from django.contrib.auth.forms import UserCreationForm
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 
-
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
